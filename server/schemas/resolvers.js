dbe205340ec5f076342251736447e9d165961051
Laura <laurawatersalston@gmail.com>
2024-08-06 18:32:58 -0400

    resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,21 +8,69 @@ const resolvers = {
     getUsers: async () => {
       return User.find();
     },
+    getSingleUser: async (_, { user, params }) => {
+      try {
+        const foundUser = await User.findOne({
+          $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+        });
+
+        return foundUser;
+      } catch (error) {
+        throw new Error('Cannot find a user with this id!');
+      }
+    },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    createUser: async (_, { body }) => {
+      try {
+        const user = await User.create(body);
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        throw new Error('Something is wrong!');
+      }
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (_, { body }) => {
+      const user = await User.findOne({ $or: [{ username: username }, { email: email }] }); 
+      if (!user) {
+        throw new Error('Cannot find this user');
+      }  
+      const correctPw = await user.isCorrectPassword(password);   
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+      const token = signToken(user);
+      return { token, user };
     },
-  },
+    saveBook: async (_, { user, body }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    deleteBook: async (_, { bookId }, { user }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+    
+        if (!updatedUser) {
+          throw new Error("Couldn't find user with this id!");
+        }
+        return updatedUser;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }, 
+  },   
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,7 @@ const typeDefs = `
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(abook: BookInput): User
+    saveBook(book: BookInput): User
     removeBook(bookId: String!): User 
   }
 

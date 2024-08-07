2fab2143ace36e13c5009e6980d6e8063dea0944
Laura <laurawatersalston@gmail.com>
2024-08-06 22:28:07 -0400

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -13,6 +13,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div>
+        <Navbar />
         <Outlet />
       </div>
     </ApolloProvider>

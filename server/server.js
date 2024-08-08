e0a23ea3d736ccf667c7e79bf03b949e07fcc837
Laura <laurawatersalston@gmail.com>
2024-08-08 11:01:14 -0400

    changes start script

--- server/server.js
+++ server/server.js
@@ -21,15 +21,6 @@ const startApolloServer = async () => {
   
   app.use('/graphql', expressMiddleware(server));
 
-  // if we're in production, serve client/dist as static assets
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../')));
-
-  //   app.get('*', (req, res) => {
-  //     res.sendFile(path.join(__dirname, '../index.html '));
-  //   });
-  // } 
-
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);

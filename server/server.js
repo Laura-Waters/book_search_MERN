a47a21ee57f7415c97e07515b9d02799ab82f734
Laura <laurawatersalston@gmail.com>
2024-08-10 20:54:57 -0400

    removes production settings

--- server/server.js
+++ server/server.js
@@ -29,14 +29,14 @@ const startApolloServer = async () => {
   );
 
 
-  // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+  // // if we're in production, serve client/dist as static assets
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
-  }
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  //   });
+  // }
 
   db.once("open", () => {
     app.listen(PORT, () => {
